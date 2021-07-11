6cda87abf53a51548e6757ba132419d11531b13e
Maciej Howański <maciej.howanski@outlook.com>
2021-07-11 16:19:18 +0200

    Bootstrap, logo, statics

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var router *gin.Engine
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	router = gin.Default()
+
+	router.LoadHTMLGlob("front-end/html/*")
+
+	router.Static("/assets/images", "./front-end/images")
+
+	router.GET("/", func(c *gin.Context) {
+
+		c.HTML(
+			http.StatusOK,
+			"index.html",
+			gin.H{
+				"title": "GOnductor",
+			},
+		)
+
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	router.Run()
 }
